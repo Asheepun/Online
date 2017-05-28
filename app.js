771dd523a62b96a91db048c081a67caf4abc76fb
Asheepun <pussagussa@gmail.com>
2017-05-28 13:27:29 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const ip = "127.0.0.1";
+
+const server = app.listen(port, ip, () => {console.log("listening on " + ip + " on port " + port);});
+
+app.use(express.static("public"));
+
+const socket = require("socket.io");
+const io = socket(server);
+
+const users = new Array();
+
+io.on("connection", socket => {
+    console.log(socket.id + " has connected!");
+
+    let data = {
+        id: socket.id,
+        users: users,
+    }
+    socket.emit("initialize", data);
+
+    socket.on("initialized", data => {
+        let olle = new User(socket.id, data.player);
+        socket.broadcast.emit("newConnection", data);
+    });
+    socket.on("update", data => {
+        socket.broadcast.emit("update", data);
+    });
+    socket.on("bullet", data => {
+        socket.broadcast.emit("bullet", data);
+    });
+    socket.on("disconnect", socket => {
+        console.log(socket);
+        users.forEach(user => {
+            if(user.id === socket.id){
+                console.log(user);
+                users.splice(users.indexOf(user), 1);
+                let data = {
+                    user: user,
+                }
+                io.sockets.emit("disconnection", data);
+            }
+        });
+    });
+});
+
+class User{
+    constructor(id, character){
+        this.id = id;
+        this.character = character;
+        users.push(this);
+    }
+}
